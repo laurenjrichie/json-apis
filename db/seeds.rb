786a18bd6b27b83c2a45a56f95ceecd62efe8f43
laurenjrichie <laurenjrichie@gmail.com>
2015-01-26 15:07:36 -0800

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create(
+  title: "A Guppy Fish Journey",
+  year: 2012,
+  plot: "Follow this fish's exciting journey through time!",
+  image_url: "http://www.aquariumfish.net/images_01/guppy_pink_varig_130328a3_w0640.jpg")
+
+Movie.create(
+  title: "Watch Fido Run",
+  year: 2011,
+  plot: "This cute three-legged dog can run really fast!",
+  image_url: "https://thejoggingdog.files.wordpress.com/2012/01/dsc_0078.jpg")
+
+Movie.create(
+  title: "I Love Balloons",
+  year: 2010,
+  plot: "The party don't stop till I walk in",
+  image_url: "http://i.imgur.com/cQ5lpbY.jpg")
+
+Movie.create(
+  title: "A Kitten Love Story",
+  year: 2009,
+  plot: "Two kittens fall in love and magic happens.",
+  image_url: "http://1funny.com/wp-content/uploads/2009/03/kittens-in-a-pot.jpg")
+
+Movie.create(
+  title: "Puppies and Sprinklers",
+  year: 2015,
+  plot: "Fun in the sun",
+  image_url: "http://netstorage.discovery.com/feeds/brightcove/asset-stills/apl/136269632131613833804501197_Lift3_Cocker_Spaniels_Sprinkler.jpg")
+
+Movie.create(
+  title: "Tales from Slothland",
+  year: 2007,
+  plot: "Yep we're cute",
+  image_url: "http://www.zooborns.com/.a/6a010535647bf3970b0168e86e2dd3970c-500wi")
+
+Movie.create(
+  title: "Hear Me Yell!",
+  year: 2005,
+  plot: "This zebra has something to say.",
+  image_url: "http://www.travelteamimages.com/pics/301/301069_big.jpg")
+
+Movie.create(
+  title: "Chewing on Wood is Fun",
+  year: 2011,
+  plot: "Nom nom nom",
+  image_url: "https://c1.staticflickr.com/3/2313/2222294539_29bd16e714.jpg")
+
+Movie.create(
+  title: "Guess What Animal",
+  year: 2006,
+  plot: "I'm a baby vulture",
+  image_url: "http://www.zooborns.com/.a/6a010535647bf3970b0133ed78f3a2970b-pi")
+
+Movie.create(
+  title: "The Itty Bitty Mousey Committee",
+  year: 2010,
+  plot: "Follow the mouse's journey through the cornfields",
+  image_url: "http://www.photobotos.com/wp-content/uploads/2013/02/Field-Mouse.jpg")
